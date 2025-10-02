94989b8800bd9e80c92dca12fdb3df0ff5772381
qkrtndls0418 <si616979@gmail.com>
2025-10-02 15:55:43 +0900

    05

--- main.c
+++ main.c
@@ -3,16 +3,38 @@
 
 int main(int argc, char *argv[])
 {
-    int x;
-    int i;
-    int sum=0;
+    int a,b;
+    char op;
+    int result;
     
-    printf("Input a number :");
-    scanf("%d", &x);
     
-    for(i=1;i<=x;i++) 
-                    sum = sum + i;
-    printf("sum is %i\n", sum);
+    printf("enter the calculation :");
+    scanf("%i%c%i", &a, &op, &b);
+    
+    switch(op)
+    {
+              case '+' : 
+                   result = a+b;
+                   break;
+                   
+              case '-' : 
+                   result = a-b;
+                   break;
+                   
+              case '*' :
+                   result = a*b;
+                   break; 
+                   
+              case '/' :
+                   result = a/b;
+                   break;
+              case '%' : 
+                   result = a%b;     
+              default : 
+                      break;
+    }
+    printf("= %i", result); 
+    
   system("PAUSE");	
   return 0;
 }
