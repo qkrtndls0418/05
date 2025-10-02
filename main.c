1c41be6fe0d179b7820811b4481152ca14e92e64
qkrtndls0418 <si616979@gmail.com>
2025-10-02 15:34:09 +0900

    05

--- main.c
+++ main.c
@@ -4,14 +4,15 @@
 int main(int argc, char *argv[])
 {
     int x;
-    printf("Input an integer :");
+    int i;
+    int sum=0;
+    
+    printf("Input a number :");
     scanf("%d", &x);
     
-    if (x > 0)
-       printf("absolute is %i.\n",x); 
-    else
-      printf("absolute is %i.\n", -x); 
-  
+    for(i=1;i<=x;i++) 
+                    sum = sum + i;
+    printf("sum is %i\n", sum);
   system("PAUSE");	
   return 0;
 }
